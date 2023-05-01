d3846205f1653ea3d38a9fdeac257ca2257c298f
xSyrax123 <99150043+xSyrax123@users.noreply.github.com>
2023-04-30 21:34:38 -0300

    Update ej2.js

--- ej2.js
+++ ej2.js
@@ -5,9 +5,9 @@ let num = 1, odd_counter = 0, result = 0;
 while (odd_counter < 20) {
   if (x % 2 === 1) {
     result += num;
-    odd_counter += 1;
+    odd_counter++;
   }
-  num += 1;
+  num++;
 }
 
 console.log(`The sum of the first 20 odd numbers is: ${result}`);
